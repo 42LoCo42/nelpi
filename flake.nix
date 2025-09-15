37e7abf33401d7a96fe8f2df7165e495957ea577
Leon Schumacher <leonsch@protonmail.com>
2025-09-15 15:50:16 +0200

    first draft; using spacy & gensim

--- flake.nix
+++ flake.nix
@@ -1,66 +1,112 @@
 {
-  outputs = { flake-utils, nixpkgs, ... }:
+  inputs = {
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { flake-utils, nixpkgs, pyproject-build-systems, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
 
-        inherit (pkgs.lib) getExe pipe readFile toLower trim;
+        inherit (pkgs.lib) composeManyExtensions getExe pipe readFile toLower trim;
         inherit (pkgs.lib.fileset) toSource unions;
+        inherit (pyproject-build-systems.inputs) pyproject-nix uv2nix;
+        inherit (pkgs.callPackages pyproject-nix.build.util { }) mkApplication;
 
-        py = pkgs.python3.pkgs;
-        proj = pipe ./pyproject.toml [ readFile fromTOML (x: x.project) ];
+        name = "nelpi";
         repo = pipe ./repo [ readFile trim toLower ];
+
+        python = pkgs.python311;
+
+        workspace = uv2nix.lib.workspace.loadWorkspace {
+          workspaceRoot = /. + (toString (builtins.unsafeDiscardStringContext (toSource {
+            root = ./.;
+            fileset = unions [
+              ./extract.py
+              ./pyproject.toml
+              ./raw
+              ./src
+              ./uv.lock
+            ];
+          })));
+        };
+
+        pythonSet =
+          (pkgs.callPackage pyproject-nix.build.packages {
+            inherit python;
+          }).overrideScope (
+            composeManyExtensions [
+              pyproject-build-systems.overlays.default
+
+              (workspace.mkPyprojectOverlay {
+                sourcePreference = "wheel";
+              })
+
+              (_: prev: {
+                ${name} = prev.${name}.overrideAttrs {
+                  preBuild = "python extract.py";
+                };
+              })
+            ]);
       in
       rec {
         packages = {
-          default = py.buildPythonApplication rec {
-            pname = proj.name;
-            inherit (proj) version;
+          inherit workspace pythonSet;
 
-            src = toSource {
-              root = ./.;
-              fileset = unions [
-                ./extract.py
-                ./raw
-
-                ./pyproject.toml
-                ./src
-              ];
-            };
-
-            preBuild = "python extract.py";
-
-            pyproject = true;
-
-            build-system = with py; [
-              setuptools
-            ];
-
-            dependencies = with py; [
-
-            ];
-
-            meta.mainProgram = pname;
+          default = mkApplication {
+            package = pythonSet.${name};
+            venv = pythonSet.mkVirtualEnv "${name}-env" workspace.deps.default;
           };
 
           image = pkgs.dockerTools.buildImage {
-            inherit (proj) name;
+            inherit name;
             config = {
               Cmd = [ (getExe packages.default) ];
               Labels = {
                 "org.opencontainers.image.source" = "https://github.com/${repo}";
-                "org.opencontainers.image.description" = "testing image";
                 "org.opencontainers.image.licenses" = "GPL-3.0-or-later";
               };
             };
           };
         };
 
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ packages.default ];
-          packages = with pkgs; with py; [
-            ipython
-          ];
-        };
+        devShells.default =
+          let
+            editableOverlay = workspace.mkEditablePyprojectOverlay {
+              root = "$REPO_ROOT";
+            };
+
+            editablePythonSet = pythonSet.overrideScope (
+              composeManyExtensions [
+                editableOverlay
+              ]);
+
+            venv = editablePythonSet.mkVirtualEnv "${name}-dev-env" workspace.deps.all;
+          in
+          pkgs.mkShell {
+            packages = with pkgs; [
+              dive
+              uv
+              venv
+            ];
+
+            env = {
+              UV_NO_SYNC = "1";
+              UV_PYTHON = python.interpreter;
+              UV_PYTHON_DOWNLOADS = "never";
+
+              LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
+                stdenv.cc.cc
+                zlib
+              ]);
+            };
+
+            shellHook = ''
+              export REPO_ROOT="$(git rev-parse --show-toplevel)"
+            '';
+          };
       });
 }
--- src/nelpi/__init__.py
+++ src/nelpi/__init__.py
@@ -1,21 +1,118 @@
+import contextlib
 import importlib.resources as resources
 import json
+import logging
 from glob import glob
-from re import match
+from math import ceil
+from re import Match, match
 
+import joblib
+import spacy
+from gensim.corpora import Dictionary
+from gensim.models import LdaMulticore
+from joblib import Parallel, delayed
+from spacy.language import Language
+from spacy.tokens import Doc
+from tqdm import tqdm
+
+NLP: Language = spacy.load("de_core_news_sm", disable=["ner", "parser"])
 
 def main() -> None:
-    datasets = glob(str(resources.files(__name__) / "datasets/*.json"))
-    entries = []
+    formatter = logging.Formatter("%(asctime)s [%(levelname)-4s] %(message)s")
 
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger("nelpi")
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+
+    info = logger.info
+
+    datasets: list[str] = sorted(glob(str(
+        resources.files(__name__) / "datasets/*.json")))
+    raw_entries: list[str] = []
+
+    info("Loading entries...")
     for path in datasets:
-        # fmt: off
-        name = match(r".*/([^/]+)\.json$", path)[1] # pyright: ignore[reportOptionalSubscript]
-        # fmt: on
+        mat: Match[str] | None = match(r".*/([^/]+)\.json$", path)
+        assert mat != None
+        name: str = mat[1]
 
-        data = json.load(open(path, "r"))
-        entries += data
-        print(f"Loaded {len(data)} entries from dataset '{name}'!")
+        data: list[str] = json.load(open(path, "r"))
+        raw_entries += data
+        info(f"  Loaded {len(data):>5} entries from dataset '{name}'")
 
-    print(f"{len(entries)} entries in total!")
-    # TODO topic identification!
+    info(f"{len(raw_entries)} entries in total!")
+
+    entries: list[list[str]] = preprocess_parallel(raw_entries)
+    dictionary: Dictionary = Dictionary(tqdm(entries, desc="Dictionary   "))
+    corpus = [dictionary.doc2bow(e) for e in tqdm(entries, desc="Corpus       ")]
+
+    info("Building LDA model...")
+    lda_model: LdaMulticore = LdaMulticore(
+        corpus     = corpus,
+        id2word    = dictionary,
+        num_topics = 10, # TODO how many topics do we want?
+        passes     = 10, # TODO is this important?
+    )
+
+    info("=" * 80)
+    for i, topic in lda_model.print_topics():
+        info(f"Topic {i}: {topic}")
+
+# https://freedium.cfd/https://medium.com/data-science/turbo-charge-your-spacy-nlp-pipeline-551435b664ad
+def chunker(list, chunksize):
+    return (list[pos : pos + chunksize] for pos in range(0, len(list), chunksize))
+
+def flatten(list_of_lists):
+    "Flatten a list of lists to a combined list"
+    return [item for sublist in list_of_lists for item in sublist]
+
+def lemmatize_pipe(doc: Doc) -> list[str]:
+    return [
+        token.lemma_.lower()
+        for token in doc
+        if not any([
+            token.is_punct,
+            token.is_space,
+            token.is_stop,
+        ])
+    ]
+
+def process_chunk(texts) -> list[list[str]]:
+    preproc_pipe: list[list[str]] = []
+    for doc in NLP.pipe(texts):
+        preproc_pipe.append(lemmatize_pipe(doc))
+    return preproc_pipe
+
+def preprocess_parallel(texts, chunksize=100) -> list[list[str]]:
+    with tqdm_joblib(tqdm(desc="Preprocessing", total=ceil(len(texts) / chunksize))):
+        executor = Parallel(n_jobs=-1, backend="multiprocessing")
+        do = delayed(process_chunk)
+        tasks = (do(chunk) for chunk in chunker(texts, chunksize=chunksize))
+        result = executor(tasks)
+        return flatten(result)
+
+# https://stackoverflow.com/a/58936697
+@contextlib.contextmanager
+def tqdm_joblib(tqdm_object):
+    """Context manager to patch joblib to report into tqdm progress bar given as argument"""
+
+    class TqdmBatchCompletionCallback(joblib.parallel.BatchCompletionCallBack):
+        def __call__(self, *args, **kwargs):
+            tqdm_object.update(n=self.batch_size)
+            return super().__call__(*args, **kwargs)
+
+    old_batch_callback = joblib.parallel.BatchCompletionCallBack
+    joblib.parallel.BatchCompletionCallBack = TqdmBatchCompletionCallback
+    try:
+        yield tqdm_object
+    finally:
+        joblib.parallel.BatchCompletionCallBack = old_batch_callback
+        tqdm_object.close()
+
+# Local Variables:
+# apheleia-mode: nil
+# lsp-inlay-hint-enable: nil
+# End:
